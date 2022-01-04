e402f22312edcadb11a645e5e31b3123a47833a2
ufukcanatan <65026462+ufukcanatan@users.noreply.github.com>
2022-01-04 19:23:12 +0300

    İlk Commit

---
+++ header.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Beylikdüzü Psikoteknik Terapi Merkezi</title>
+    <meta content="" name="descriptison">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- ======= Top Bar ======= -->
+    <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top ">
+        <div class="container d-flex">
+            <div class="contact-info mr-auto">
+                <i class="icofont-envelope"></i> <a href="mailto:dr.hanifeyilmazcengel@gmail.com">Mail ile ulaşın</a>
+                <i class="icofont-phone"></i> <a href="tel:+905363262628">Arayarak ulaşın</a>
+            </div>
+            <div class="social-links">
+                <a href="https://www.instagram.com/yolpsikoterapimerkezi/" target="_blank" class="instagram"><i class="icofont-instagram"></i></a>
+                <a href="https://wa.me/+905436888041" target=”_blank” class="whatsapp"><i class="icofont-brand-whatsapp"></i></a>
+                <a href="https://goo.gl/maps/MroUB2rWotS4EdaC7" target="_blank" class="maps"><i class="icofont-google-map"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="whatsapp-top w-100 d-flex">
+        <div id="whatsapp" class="whatsapp">
+            <a href="https://wa.me/+905436888041" target=”_blank” class="whatsapp"><i class="icofont-brand-whatsapp"></i> İletişime Geçin</a>
+        </div>
+    </div>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top ">
+        <div class="container d-flex align-items-center">
+
+            <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+
+            <nav class="nav-menu d-none d-lg-block">
+                <ul>
+                    <li class="active"><a href="index.html">Anasayfa</a></li>
+                    <li><a href="#about">Hakkımızda</a></li>
+                    <li><a href="#services">Terapiler</a></li>
+                    <li><a href="#portfolio">Tedaviler</a></li>
+                    <li><a href="#pricing">Hastalıklar</a></li>
+                    <!-- <li><a href="#team">Blog</a></li> -->
+                    <!-- <li class="drop-down"><a href="">Drop Down</a>
+                        <ul>
+                            <li><a href="#">Drop Down 1</a></li>
+                            <li class="drop-down"><a href="#">Deep Drop Down</a>
+                                <ul>
+                                    <li><a href="#">Deep Drop Down 1</a></li>
+                                    <li><a href="#">Deep Drop Down 2</a></li>
+                                    <li><a href="#">Deep Drop Down 3</a></li>
+                                    <li><a href="#">Deep Drop Down 4</a></li>
+                                    <li><a href="#">Deep Drop Down 5</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Drop Down 2</a></li>
+                            <li><a href="#">Drop Down 3</a></li>
+                            <li><a href="#">Drop Down 4</a></li>
+                        </ul>
+                    </li> -->
+                    <li><a href="#contact">İletişim</a></li>
+
+                </ul>
+            </nav>
+            <!-- .nav-menu -->
+
+        </div>
+    </header>
